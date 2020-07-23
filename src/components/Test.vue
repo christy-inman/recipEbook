48e2c12764c63d8d852c3bb3245fb5a002ccc366
github username <github email address>
2020-07-23 11:48:00 -0500

    Set up, and first test component with data.

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>TESTING</h1>
+        <p>{{sampleMessage}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+      sampleMessage: String
+  }
+}
+</script>
